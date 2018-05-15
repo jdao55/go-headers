88a311597665d5172be600e0b946cb76879712c5
jdao55 <joedao55@gmail.com>
2018-05-15 19:57:14 -0400

    Added basic functionality

--- main.go
+++ main.go
@@ -4,17 +4,59 @@ import (
 	"fmt"
 	"os"
 	"net/http"
+	"time"
+	"bytes"
+	"text/tabwriter"
+	"sort"
+	//"strings"
 )
 
-func headerToString(header http.Header) string {
-	headerString := ""
-	return headerString
+func checkRedirect(req *http.Request, via []*http.Request) error {
+	return http.ErrUseLastResponse
 }
+
+
+func PrintHeader(resp *http.Header) {
+	var (
+		buffer bytes.Buffer
+		tab_writer = tabwriter.NewWriter(&buffer, 0, 0, 2, ' ', 0)
+	)
+	header_list := make([]string, 0, len(*resp))
+	for header_key := range *resp{
+		header_list = append(header_list, header_key)
+	}
+
+	sort.Strings(header_list)
+
+	
+	for _, header_key := range header_list[:] {
+		for _, headerValue := range (*resp)[header_key] {
+
+			if len(headerValue)>60 {
+				fmt.Fprintf(tab_writer, "%s\t%s\n", header_key, headerValue[:60])
+				headerValue = headerValue[60:]
+				for len(headerValue) > 60 {
+					fmt.Fprintf(tab_writer, "\t%s\n", headerValue[:60])
+					headerValue = headerValue[60:]
+				}
+				fmt.Fprintf(tab_writer, "\t%s\n",  headerValue)
+			} else {
+					fmt.Fprintf(tab_writer, "%s\t%s\n", header_key, headerValue)
+			}
+			
+}
+	}
+
+	tab_writer.Flush()
+	fmt.Println(buffer.String())
+}
+
 
 
 func main() {
 
 	var client = http.Client{
+		CheckRedirect: checkRedirect,
 		Transport: &http.Transport{
 			DisableCompression: true,
 		},
@@ -25,10 +67,18 @@ func main() {
 	if err!=nil {
 		os.Exit(1)
 	}
+
+	start_time := time.Now()
 	resp, err := client.Do(req)
-	fmt.Println(resp.Header)
+	req_duration := time.Since(start_time)
+
 	if err!=nil {
 		os.Exit(1)
 	}
+	fmt.Println("");
+	fmt.Println("GET ", url)
+	fmt.Println("Took: ", req_duration)
+	fmt.Println(resp.Proto, resp.Status)
+	PrintHeader(&(resp.Header))
 	os.Exit(0)	
 }
